8bebcfd2763de2c81c7285c6ddb79c3bb4cc9434
niko caetano <ellercaetano@gmail.com>
2022-06-22 22:11:50 -0300

    atividade concluída

---
+++ script.js
@@ -0,0 +1,30 @@
+ // Crie um array que receba 5 itens e exiba no console.
+
+let things = ['Ludmilla', 'Anitta', 'Majur', 'Demi', 'Iza'];
+console.log(things)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+things.unshift('Urias');
+console.log(things)
+
+// Utilize um método para remover o último nome do array.
+
+things.pop();
+console.log(things)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+things.push('Linn', 'Manu');
+console.log(things)
+
+// Utilize um método para remover o primeiro nome do array.
+
+things.shift();
+console.log(things)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers.sort())
